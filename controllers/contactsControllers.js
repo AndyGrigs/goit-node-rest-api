f864f317faea50ff5ceb9b47d3f900ea358bb9b9
Andriy <andygrigs88@gmail.com>
2025-11-10 15:35:32 +0100

    fixed app

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,70 +1,71 @@
 import * as contactsServices from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
 
-export async function getAllContacts(req, res) {
+export async function getAllContacts(req, res, next) {
   try {
     const contacts = await contactsServices.listContacts();
     // Validate that it's an array
     if (!Array.isArray(contacts)) {
-      throw new Error("contacts.json must contain an array");
+      throw HttpError(500, "contacts.json must contain an array");
     }
     res.json(contacts);
   } catch (error) {
-    console.error("Error reading contacts:", error.message);
-    throw error;
+    next(error);
   }
 }
 
-export const getContact = async (req, res) => {
+export const getContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const contact = await contactsServices.getContactById(id);
 
     if (!contact) {
-      return res.status(404).json({ message: "Not found" });
+      throw HttpError(404, "Not found");
     }
 
     res.status(200).json(contact);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
 
-export const removeContact = async (req, res) => {
+export const removeContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const contact = await contactsServices.deleteContact(id);
 
     if (!contact) {
-      return res.status(404).json({ message: "Not found" });
+      throw HttpError(404, "Not found");
     }
 
-    res.status(200).json({message: "Contact deleted!"});
+    
+    res.status(200).json(contact);
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
 
-export const postContact = async (req, res) => {
+export const postContact = async (req, res, next) => {
   try {
     const { name, email, phone } = req.body;
     const newContact = await contactsServices.addContact(name, email, phone);
     res.status(201).json(newContact);
-  } catch (err) {
-    res.status(500).json({ message: 'Internal server error' });
+  } catch (error) {
+    next(error);
   }
 };
 
-export const putContact = async (req, res) => {
+export const putContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const updatedContact = await contactsServices.updateContact(id, req.body);
 
     if (!updatedContact) {
-      return res.status(404).json({ message: "Not found" });
+      throw HttpError(404, "Not found");
     }
 
     res.status(200).json(updatedContact);
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error' });
+    next(error);
   }
 };
--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-import express from 'express';
-import contactsRouter from './routes/contactsRouter.js';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
 
+app.use(morgan("tiny"));
+app.use(cors());
 app.use(express.json());
 
-app.use('/contacts', contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
-// Error handler middleware
+
 app.use((err, _, res, __) => {
-  const { status = 500, message = 'Internal server error' } = err;
+  const { status = 500, message = "Internal server error" } = err;
   res.status(status).json({ message });
 });
 
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
 app.listen(3000, () => {
- console.log('Example app is listening on port 3000.');
+  console.log("Server is listening on port 3000.");
 });
