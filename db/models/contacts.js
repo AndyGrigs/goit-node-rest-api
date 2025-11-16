bac1d13f52347b2a4b6ab158867a618131e043c1
Andriy <andygrigs88@gmail.com>
2025-11-16 23:18:47 +0100

    edit model

--- db/models/contacts.js
+++ db/models/contacts.js
@@ -4,11 +4,6 @@ import sequelize from "../sequelize.js";
 const Contact = sequelize.define(
   "Contact",
   {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
     name: {
       type: DataTypes.STRING(255),
       allowNull: false,
