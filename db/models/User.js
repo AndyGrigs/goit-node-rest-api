c715659f9b2df0d4453a48d8b6a5fffd0f9c9ee4
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-11-20 13:28:49 +0100

    add user model

---
+++ db/models/User.js
@@ -0,0 +1,41 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../sequelize.js";
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primryKey: true,
+    autoIcrement: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: {
+        msg: "Invalid email format",
+      },
+    },
+  },
+  subscription: {
+    type:DataTypes.ENUM("starter", "pro", "business"),
+    defaultValue: "starter",
+  },
+  token: {
+    type:DataTypes.STRING,
+    defaultValue: null,
+  },
+},
+{
+    tableName: "users",
+    timestamps: true,
+    underscored: true, // created_at, updated_at
+  }
+);
+
+export default User;
