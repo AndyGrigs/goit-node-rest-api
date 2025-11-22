4d5cb7e67d5c4b787ab052b810ecf5e23863e2d0
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-11-22 13:50:15 +0100

    add auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,31 @@
-import * as authServices from "";
+import * as authServices from "../services/authServices.js";
+import HttpError from "../helpers/HttpError.js";
+
+export const register = async (req, res, next) =>{
+    try {
+        const {email, password} = req.body;
+        const result = await authServices.registerUser(email, password);
+        if(result.error){
+            throw HttpError(result.status, result.error);
+        }
+    res.status(201).json(result);
+
+    } catch (error) {
+        next(error)
+    }
+};
+
+export const login = async (req, res, next) =>{
+    try {
+        const {email, password} = req.body;
+        const result = await authServices.loginUser(email, password);
+        
+        if(result.error){
+            throw HttpError(result.status, result.error);
+        }
+    res.status(201).json(result);
+
+    } catch (error) {
+        next(error)
+    }
+};
